2a3a8be329ad58cd91dac65bd0f2aa66518a6f3d
meim234 <meim54504@gmail.com>
2025-02-13 14:51:42 +0000

    Add files via upload

---
+++ mylove.js
@@ -0,0 +1,70 @@
+let messages = ["Please?", "Give me a chance to be your wife", "Can I be your wife please?", "You are love of my life", "I want to be your wife"];
+let noCount = 0;
+let noButton = document.getElementById("no");
+let yesButton = document.getElementById("yes");
+let messageText = document.getElementById("message");
+
+noButton.addEventListener("click", rejectLove);
+yesButton.addEventListener("click", acceptLove);
+
+function rejectLove() {
+    if (noCount < messages.length) {
+        messageText.innerText = messages[noCount];
+        noCount++;
+        noButton.style.transform = `scale(${1 - noCount * 0.1})`;
+        yesButton.style.transform = `scale(${1 + noCount * 0.1})`;
+    }
+    if (noCount === messages.length) {
+        noButton.style.display = "none";
+    }
+}
+
+function acceptLove() {
+    document.getElementById("valentine").innerHTML = `
+        <img src="https://media.tenor.com/oWTGv7XpHY4AAAAi/sseeyall-bubu-dudu.gif" class="gif">
+        <div class="question">Yayyy... I love you so much my sweetie❤️</div>
+        <img src="https://media.tenor.com/fHZ8Z_i5ngIAAAP1/kiss-attack.mp4" class="gif">
+        <div class="question"> Muahhhh.... 😽 We are always hubby and wifey</div>
+    `;
+    launchConfetti();
+    startHeartRain();
+}
+
+function launchConfetti() {
+    var duration = 3 * 1000;
+    var end = Date.now() + duration;
+
+    (function frame() {
+        confetti({
+            particleCount: 5,
+            angle: 60,
+            spread: 55,
+            origin: { x: 0 }
+        });
+        confetti({
+            particleCount: 5,
+            angle: 120,
+            spread: 55,
+            origin: { x: 1 }
+        });
+
+        if (Date.now() < end) {
+            requestAnimationFrame(frame);
+        }
+    })();
+}
+
+function startHeartRain() {
+    const heartContainer = document.getElementById("heart-container");
+    setInterval(() => {
+        let heart = document.createElement("div");
+        heart.classList.add("heart");
+        heart.innerHTML = "❤️";
+        heart.style.left = Math.random() * window.innerWidth + "px";
+        heartContainer.appendChild(heart);
+
+        setTimeout(() => {
+            heart.remove();
+        }, 5000);
+    }, 300);
+}
